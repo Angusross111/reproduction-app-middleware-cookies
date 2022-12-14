cac81703455ef25da8407b5830c6de7cfda60129
Angus Ross <angus.ross@swarmfarm.com>
2022-12-15 08:49:44 +1100

    Remove logging from middleware

--- middleware.ts
+++ middleware.ts
@@ -4,10 +4,8 @@ import { NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
     const cookie = req.cookies.get("vercel-cookie")?.value;
-    console.log("middleware:cookie", cookie);
     if (cookie) return NextResponse.next();
     const response = NextResponse.next();
     response.cookies.set("vercel-cookie", "cookie-data", { maxAge: 30 });
-    console.log("response", response);
     return response;
 }
