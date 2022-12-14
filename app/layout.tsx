3f85643626e1bb5ea4d14b8ca139185404d91c74
Angus Ross <angus.ross@swarmfarm.com>
2022-12-15 08:43:26 +1100

    Testing middleware with cookies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,25 @@
-import './globals.css'
+import { cookies, headers } from "next/headers";
+import { notFound } from "next/navigation";
 
-export default function RootLayout({
-  children,
+import "./globals.css";
+
+export default async function RootLayout({
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      {/*
+    headers().forEach((header) => console.log("header", header));
+    const vercelCookie = cookies().get("vercel-cookie")?.value;
+    console.log("vercelCookie", vercelCookie);
+    if (!vercelCookie) notFound();
+    return (
+        <html lang="en">
+            {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+            <head />
+            <body>{children}</body>
+        </html>
+    );
 }
